23029b90803735fcfd1a85671bcc273210e5ce92
Casper van den Berg <casper.van.den.berg@student.nhlstenden.com>
2020-06-16 18:59:11 +0200

    make it better

--- src/main/java/org/thethingsnetwork/samples/mqtt/App.java
+++ src/main/java/org/thethingsnetwork/samples/mqtt/App.java
@@ -5,29 +5,32 @@ import org.thethingsnetwork.data.mqtt.Client;
 
 import java.net.URISyntaxException;
 
-public class App {
+public class App
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         String region = "eu";
         String appId = "co2_sensor_stenden";
         String accessKey = "ttn-account-v2.J5ws5KGhK9jVP5p56HfG1VyLka8PecrVTtIsam6MpWA";
 
-        try{
+        try
+        {
             Client client = new Client(region, appId, accessKey);
             client.onError((Throwable _error) -> System.err.println("error: " + _error.getMessage()));
 
             client.onConnected((Connection _client) -> System.out.println("connected!"));
 
             client.start();
-        }catch (URISyntaxException ex) {
+        } catch (URISyntaxException ex)
+        {
             ex.printStackTrace();
             System.out.println("");
             System.out.println(ex.getMessage());
-        }catch (Exception ex)
+        } catch (Exception ex)
         {
             ex.printStackTrace();
         }
-
 
 
     }
