06ea7d30c8c78418ca281c3f79bf25803ee338af
Casper <cms.vandenberg@gmail.com>
2020-06-23 18:13:40 +0200

    Updated the app and pom file

--- src/main/java/org/thethingsnetwork/samples/mqtt/App.java
+++ src/main/java/org/thethingsnetwork/samples/mqtt/App.java
@@ -1,6 +1,10 @@
 package org.thethingsnetwork.samples.mqtt;
 
 import org.json.JSONObject;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.thethingsnetwork.data.common.Connection;
 import org.thethingsnetwork.data.common.messages.ActivationMessage;
 import org.thethingsnetwork.data.common.messages.DataMessage;
@@ -13,53 +17,41 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
+@Controller
+@SpringBootApplication
 public class App
 {
-
     public static void main(String[] args)
     {
         String region = "eu";
         String appId = "co2_sensor_stenden";
-        String accessKey = "ttn-account-v2.J5ws5KGhK9jVP5p56HfG1VyLka8PecrVTtIsam6MpWA";
+        String accessKey = "ttn-account-v2.J5ws5KGhK9jVP5p56HfG1VyLka8PecrVTtIsam6MpWA"; // Top-Secret
         String apiUrl = "https://loradashboardapi.herokuapp.com/add_measurement";
 
         try
         {
             Client client = new Client(region, appId, accessKey);
 
-            class Response {
-
-                private String message;
-
-                public Response(String _message) {
-                    message = _message;
-                }
-            }
-
-//            client.onMessage(null, "co2", (String _devId, DataMessage _data) -> {
-//                try {
-//                    RawMessage message = (RawMessage) _data;
-//                    // Toggle the LED
-////                    DownlinkMessage response = new DownlinkMessage(0, new Response(!message.asBoolean()));
-//                    DownlinkMessage response = new DownlinkMessage(0, new Response(message.asString()));
-//
-//                    /**
-//                     * If you don't have an encoder payload function:
-//                     * client.send(_devId, new Response(0, message.asBoolean() ? new byte[]{0x00} : new byte[]{0x01}));
-//                     */
-//                    System.out.println("Sending: " + _data.toString());
-//                    client.send(_devId, response);
-//                } catch (Exception ex) {
-//                    System.out.println("Response failed: " + ex.getMessage());
-//                }
-//            });
-
             client.onMessage((String devId, DataMessage data) -> {
 
                 try
                 {
                     UplinkMessage message = (UplinkMessage) data;
-                    String jsonMessage = new JSONObject(message.getPayloadFields()).toString();
+                    int co2 = (int)message.getPayloadFields().get("co2");
+                    double temperature = (double)message.getPayloadFields().get("temperature");
+                    double humidity = (double)message.getPayloadFields().get("humidity");
+                    int tvoc = (int)message.getPayloadFields().get("tvoc"); // hydrocarbons
+                    String DateTime = java.time.LocalDateTime.now().toString();
+                    JSONObject Obj = new JSONObject();
+                    Obj.put("datetime", DateTime);
+                    Obj.put("air_pressure", 0); // Node doesn't support air pressure
+                    Obj.put("humidity", humidity);
+                    Obj.put("temperature", temperature);
+                    Obj.put("hydrocarbons", tvoc);
+                    Obj.put("carbon_dioxide", co2);
+                    int nodeId = Integer.parseInt(devId);
+                    Obj.put("nodeID", nodeId);
+                    String jsonMessage = Obj.toString();
                     System.out.println("Message json: " + (jsonMessage));
 
                     HttpURLConnection conn = (HttpURLConnection) new URL(apiUrl).openConnection();
@@ -69,7 +61,7 @@ public class App
                     try(OutputStream os = conn.getOutputStream()) {
                         byte[] input = jsonMessage.getBytes(StandardCharsets.UTF_8);
                         os.write(input, 0, input.length);
-                        System.out.println("Message sent to DB");
+                        System.out.println("Message sent to API");
                     }catch (Exception out) {
                         System.out.println("Something went wrong with sending the message!");
                     }
@@ -86,21 +78,23 @@ public class App
 
             client.onError((Throwable _error) -> System.err.println("error: " + _error.getMessage()));
 
-            client.onConnected((Connection _client) -> System.out.println("connected !"));
+            client.onConnected((Connection _client) -> System.out.println("connected!"));
 
             client.start();
 
         } catch (URISyntaxException ex)
         {
             ex.printStackTrace();
-            System.out.println("");
             System.out.println(ex.getMessage());
         } catch (Exception ex)
         {
             ex.printStackTrace();
         }
-
-
+        SpringApplication.run(App.class, args);
     }
 
+    @RequestMapping("/")
+    public String index() {
+        return "Server is running!";
+    }
 }
